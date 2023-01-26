5ca5f5b81f7689c40bd55a1675945fe9eb7f2ba4
Dsaquel <noblet.ouwaysgta5@gmail.com>
2023-01-26 20:24:03 +0100

    filter in table

--- src/views/tournament/TournamentRequests.vue
+++ src/views/tournament/TournamentRequests.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { ElTable } from 'element-plus';
-import { Staff } from '~/types';
+import { Role, Staff } from '~/types';
 
 const { fetchTournament, fetchControlAccess } = tournamentStore();
 const { tournament, isAuthorized, access, staffRequests } = storeToRefs(tournamentStore());
@@ -26,8 +26,9 @@ const goBack = () => {
   });
 };
 
-function filterRequests(value: string, row: Staff) {}
-// filter / select for delete or adding
+function filterRequests(value: Role, row: Staff & { source: Role }) {
+  return value === row.source;
+}
 </script>
 
 <template>
@@ -35,47 +36,54 @@ function filterRequests(value: string, row: Staff) {}
     <template #description>Only admins have access to this page</template>
   </el-empty>
   <div v-else-if="tournament" grid="~ cols-5 gap-4">
-    <div grid="col-span-4">
-      <el-table ref="tableStaff" :data="staffRequests" row-key="id" :lazy="true" height="max-content">
-        <el-table-column type="selection" width="55" />
+    <el-table ref="tableStaff" grid="col-span-4" :data="staffRequests">
+      <el-table-column type="selection" width="55" />
 
-        <el-table-column label="name">
-          <template #default="scope"> {{ scope.row.user.username }} </template>
-        </el-table-column>
+      <el-table-column label="name">
+        <template #default="scope"> {{ scope.row.user.username }} </template>
+      </el-table-column>
 
-        <el-table-column label="profiles">
-          <template #default="scope">
-            <a :href="`https://osu.ppy.sh/users/${scope.row.user.osuId}`" target="_blank">
-              <el-button type="primary" size="small" link>osu profile</el-button>
-            </a>
-            |
-            <router-link :to="{ name: 'user-profile', params: { userId: scope.row.user.id } }"
-              >osuHow profile
-            </router-link>
-          </template>
-        </el-table-column>
+      <el-table-column label="profiles">
+        <template #default="scope">
+          <a :href="`https://osu.ppy.sh/users/${scope.row.user.osuId}`" target="_blank">
+            <el-button type="primary" size="small" link>osu profile</el-button>
+          </a>
 
-        <el-table-column
-          label="request for"
-          :filter-method="filterRequests"
-          :filters="[{ text: 'Mappooler', value: 'Mappooler' }]"
-        ></el-table-column>
+          <router-link :to="{ name: 'user-profile', params: { userId: scope.row.user.id } }"
+            >osuHow profile
+          </router-link>
+        </template>
+      </el-table-column>
 
-        <el-table-column label="actions" align="right" min-width="100%">
-          <template #default="scope">
-            <el-tooltip content="accept" placement="left">
-              <el-button type="success" size="small" round m="l">
-                <i-ic:round-check />
-              </el-button>
-            </el-tooltip>
+      <el-table-column
+        label="request for"
+        :filter-method="filterRequests"
+        :filters="[
+          { text: 'admin', value: 'admin' },
+          { text: 'mappooler', value: 'mappooler' },
+          { text: 'referee', value: 'referee' },
+        ]"
+      >
+        <template #default="scope">
+          <el-tag>{{ scope.row.source }}</el-tag>
+        </template>
+      </el-table-column>
 
-            <el-tooltip content="remove" placement="right">
-              <el-button type="danger" size="small" round m="l-1"><i-akar-icons:cross /> </el-button>
-            </el-tooltip>
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
+      <el-table-column label="actions" align="right">
+        <template #default>
+          <el-tooltip content="accept" placement="left">
+            <el-button type="success" size="small" round m="l">
+              <i-ic:round-check />
+            </el-button>
+          </el-tooltip>
+
+          <el-tooltip content="remove" placement="right">
+            <el-button type="danger" size="small" round m="l-1"><i-akar-icons:cross /> </el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
+
     <TournamentStaff :tournament-id="tournament.id" />
   </div>
   <el-empty v-else>
