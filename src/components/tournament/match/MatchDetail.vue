ae9d09a9912f643df821f1783a74cdba2d091160
Dsaquel <noblet.ouwaysgta5@gmail.com>
2023-02-10 11:50:24 +0100

    :construction: match detail with tabs

--- src/components/tournament/match/MatchDetail.vue
+++ src/components/tournament/match/MatchDetail.vue
@@ -1,47 +1,73 @@
 <script setup lang="ts">
 import { Match } from '~/types';
 
-defineProps<{
+const { access } = storeToRefs(tournamentStore());
+
+const props = defineProps<{
   match: Match;
 }>();
+const { match } = toRefs(props); // TODO:  desactive refs after testing
+const activeTab = ref('matchDetail');
 </script>
 
 <template>
-  <el-dialog v-bind="useAttrs()" append-to-body>
-    <el-row justify="space-between">
-      <el-col :span="10">
-        <el-card shadow="never">
-          <div flex="~" align="items-center" justify="between">
-            <div flex="~ col" align="items-center">
-              <el-avatar size="large" :src="match.player1.user.avatarUrl" />
-              <span text="base">{{ match.player1.user.username }}</span>
-            </div>
-            <span text="3xl">{{ match.player1Score || 0 }}</span>
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :span="4">
-        <div flex="~" align="items-center" h="full" justify="center">
-          <span text="3xl">versus</span>
-          <div></div>
+  <el-dialog v-bind="useAttrs()" append-to-body align-center :show-close="false">
+    <template #header>
+      <div flex="~" justify="between">
+        <div>Match {{ match.identifier }}</div>
+        <div flex="~" align="items-center">
+          <i-tabler:point-filled
+            :class="{
+              'text-red-600': match.state === 'playing',
+              'text-green-500': match.state === 'complete',
+              'text-orange-500': match.state === 'pending',
+            }"
+          />
+          <span font="semibold">
+            {{ match.state === 'playing' ? 'live' : match.state === 'complete' ? 'finished' : match.state }}
+          </span>
         </div>
-      </el-col>
-      <el-col :span="10">
-        <el-card shadow="never">
-          <div flex="~" align="items-center" justify="between">
-            <span text="3xl">{{ match.player1Score || 0 }}</span>
-            <div flex="~ col" align="items-center">
-              <el-avatar size="large" :src="match.player2.user.avatarUrl" />
-              <span text="base">{{ match.player2.user.username }}</span>
+      </div>
+    </template>
+    <el-tabs v-model="activeTab">
+      <el-tab-pane label="Detail" name="matchDetail">
+        <el-row justify="space-between">
+          <el-col :span="10">
+            <el-card shadow="never">
+              <div flex="~" align="items-center" justify="between">
+                <div flex="~ col" align="items-center">
+                  <el-avatar size="large" :src="match.player1.user.avatarUrl" />
+                  <span text="base">{{ match.player1.user.username }}</span>
+                </div>
+                <span text="3xl">{{ match.player1Score || 0 }}</span>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :span="4">
+            <div flex="~" align="items-center" h="full" justify="center">
+              <span text="3xl">versus</span>
             </div>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-    <!-- voir quand est ce que le match commmence ainsi que les autres infos -->
-    <!-- pouvoir edit le match "superReferee like" -->
-    <!-- pouvoir reschedule -->
+          </el-col>
+          <el-col :span="10">
+            <el-card shadow="never">
+              <div flex="~" align="items-center" justify="between">
+                <span text="3xl">{{ match.player1Score || 0 }}</span>
+                <div flex="~ col" align="items-center">
+                  <el-avatar size="large" :src="match.player2.user.avatarUrl" />
+                  <span text="base">{{ match.player2.user.username }}</span>
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-tab-pane>
 
-    <MatchUpdate :match="match" />
+      <!-- voir quand est ce que le match commmence ainsi que les autres infos -->
+      <!-- pouvoir edit le match "superReferee like" -->
+      <!-- pouvoir reschedule -->
+      <el-tab-pane v-if="access?.isAdmin || access?.isReferee || access?.isOwner" label="Update" name="matchUpdate">
+        <MatchUpdate :match="match" />
+      </el-tab-pane>
+    </el-tabs>
   </el-dialog>
 </template>
--- src/components/tournament/match/MatchUpdate.vue
+++ src/components/tournament/match/MatchUpdate.vue
@@ -1,18 +1,9 @@
 <script lang="ts" setup>
 import { Match } from '~/types';
 
-const { access } = storeToRefs(tournamentStore());
-
 defineProps<{
   match: Match;
 }>();
-
-const showMatchUpdate = ref(false);
 </script>
 
-<template>
-  <el-button v-if="access?.isAdmin || access?.isReferee || access?.isOwner" @click="showMatchUpdate = true"
-    >oui
-  </el-button>
-  <el-dialog v-model="showMatchUpdate" draggable> match update desu ! </el-dialog>
-</template>
+<template>update match desu</template>
