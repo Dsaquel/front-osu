65c6681055d5728907c161d355bdc464e83ca060
Dsaquel <noblet.ouwaysgta5@gmail.com>
2023-02-10 17:50:33 +0100

    remove file match update

--- src/components/tournament/match/MatchDetail.vue
+++ src/components/tournament/match/MatchDetail.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import dayjs from 'dayjs';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+import utc from 'dayjs/plugin/utc';
 import { Match } from '~/types';
 
+dayjs.extend(utc);
 dayjs.extend(LocalizedFormat);
 
 const { access } = storeToRefs(tournamentStore());
@@ -10,8 +12,52 @@ const { access } = storeToRefs(tournamentStore());
 const props = defineProps<{
   match: Match;
 }>();
+
+const firstToOptions = [5, 6, 7];
+const stateOptions = ['pending', 'playing', 'complete'];
+
 const { match } = toRefs(props); // TODO:  desactive refs after testing
 const activeTab = ref('matchDetail');
+
+const firstToTemplate = ref(props.match.firstTo);
+const player1ScoreTemplate = ref(props.match.player1Score);
+const player2ScoreTemplate = ref(props.match.player2Score);
+const matchesHistoryOsuTemplate = ref(props.match.matchesHistoryOsu);
+const stateTemplate = ref(props.match.state);
+const startDateTemplate = ref(props.match.startDate);
+const rulesLobbyTemplate = ref(props.match.rulesLobby ?? '');
+
+let cannotUpdate = $ref(true);
+let updateLoading = $ref(false);
+
+watch(
+  [
+    firstToTemplate,
+    player1ScoreTemplate,
+    player2ScoreTemplate,
+    matchesHistoryOsuTemplate,
+    matchesHistoryOsuTemplate,
+    stateTemplate,
+    startDateTemplate,
+    rulesLobbyTemplate,
+  ],
+  () => {
+    cannotUpdate = false;
+  },
+  { immediate: false },
+);
+async function updateMatch() {
+  try {
+    updateLoading = true;
+    // call update
+    ElMessage({ type: 'success', message: 'match updated', duration: 1000 });
+  } catch (e) {
+    ElMessage({ message: `error: ${e}`, duration: 1000 });
+  } finally {
+    updateLoading = false;
+    cannotUpdate = true;
+  }
+}
 </script>
 
 <template>
@@ -66,16 +112,81 @@ const activeTab = ref('matchDetail');
         </el-row>
       </el-tab-pane>
 
-      <!-- voir quand est ce que le match commmence ainsi que les autres infos -->
-      <!-- pouvoir edit le match "superReferee like" -->
-      <!-- pouvoir reschedule -->
       <el-tab-pane v-if="access?.isAdmin || access?.isReferee || access?.isOwner" label="Update" name="matchUpdate">
-        <MatchUpdate :match="match" />
+        <div p="2" border="b-1 b-dark-50 opacity-60">
+          <div text="lg center">Points</div>
+          <div grid="~ cols-2" align="items-center">
+            <div flex="~" align="items-center">
+              <div flex="~ col" align="items-center" p="2">
+                <el-avatar :src="match.player1.user.avatarUrl" />
+                <span text="base">{{ match.player1.user.username }}</span>
+              </div>
+              <el-input-number v-model="player1ScoreTemplate" :min="0" :max="props.match.firstTo" />
+            </div>
+
+            <div justify="end" flex="~" align="items-center">
+              <el-input-number v-model="player2ScoreTemplate" :min="0" :max="props.match.firstTo" />
+              <div flex="~ col" align="items-center" p="2">
+                <el-avatar :src="match.player2.user.avatarUrl" />
+                <span text="base">{{ match.player2.user.username }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div grid="~ cols-4 gap-3" m="t-4">
+          <div>
+            <span display="block" text="xs">first to</span>
+            <el-select v-model="firstToTemplate" filterable allow-create>
+              <el-option-group label="first to" tag-type="success">
+                <el-option
+                  v-for="(item, v) in firstToOptions"
+                  :key="v"
+                  :value="item"
+                  :disabled="item === firstToTemplate"
+                />
+              </el-option-group>
+            </el-select>
+          </div>
+          <div>
+            <span display="block" text="xs">matches history link</span>
+            <el-input v-model="matchesHistoryOsuTemplate" />
+          </div>
+          <div>
+            <span display="block" text="xs">state of match</span>
+            <el-select v-model="stateTemplate">
+              <el-option v-for="(item, v) in stateOptions" :key="v" :value="item" :disabled="item === stateTemplate" />
+            </el-select>
+          </div>
+
+          <CommonDatepicker
+            :model-value="startDateTemplate"
+            type="datetime"
+            title="start of match"
+            @update:model-value="(val) => (startDateTemplate = dayjs(val).utc().format())"
+          />
+          <div grid="col-span-4" justify="self-center" class="sm:w-min-80">
+            <span display="block" text="sm">rules lobby (if exist)</span>
+            <el-input
+              v-model="rulesLobbyTemplate"
+              type="textarea"
+              :autosize="{ minRows: 3, maxRows: 6 }"
+              w="min-[80%]"
+            />
+          </div>
+        </div>
       </el-tab-pane>
     </el-tabs>
 
     <template #footer>
-      {{ match.startDate ? 'schedule: ' + dayjs(match.startDate).format('LLLL') : 'unscheduled yet' }}
+      <template v-if="activeTab === 'matchDetail'">
+        {{ match.startDate ? 'schedule: ' + dayjs(match.startDate).format('LLLL') : 'unscheduled yet' }}
+      </template>
+      <template v-if="activeTab === 'matchUpdate'">
+        <el-button type="success" :disabled="cannotUpdate" :loading="updateLoading" @click="updateMatch"
+          >Update</el-button
+        >
+      </template>
     </template>
   </el-dialog>
 </template>
