e49888be09db40871d04a5458451128c201b9f27
Dsaquel <noblet.ouwaysgta5@gmail.com>
2023-05-15 13:40:32 +0200

    fix login

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import en from 'element-plus/lib/locale/lang/en';
 
-onBeforeMount(() => {
+onBeforeMount(async () => {
   const cookie = (new RegExp(`${'cookie' || '='}=(.*?); `, 'gm').exec(`${document.cookie}; `) || ['', null])[1];
   if (cookie) {
-    userStore().fetch();
+    try {
+      await userStore().fetch();
+    } catch (e) {
+      console.log(e);
+      window.open(
+        import.meta.env.PROD ? import.meta.env.VITE_OSU_PROD_CALLBACK_URL : import.meta.env.VITE_OSU_DEV_CALLBACK_URL,
+        '_self',
+      );
+    }
   }
 });
 
