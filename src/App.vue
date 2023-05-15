c54852a5f9b399179b1737d9f949c4221c9c2d31
Dsaquel <noblet.ouwaysgta5@gmail.com>
2023-05-15 20:44:15 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts">
 import en from 'element-plus/lib/locale/lang/en';
 
+const reSignin = async (retry?: number) => {
+  if (retry) return;
+
+  await authStore().logout();
+  document.cookie = 'cookie=; Max-Age=0';
+  window.open(
+    import.meta.env.PROD ? import.meta.env.VITE_OSU_PROD_CALLBACK_URL : import.meta.env.VITE_OSU_DEV_CALLBACK_URL,
+    '_self',
+  );
+};
+
 onBeforeMount(async () => {
   const cookie = (new RegExp(`${'cookie' || '='}=(.*?); `, 'gm').exec(`${document.cookie}; `) || ['', null])[1];
   if (cookie) {
     try {
       await userStore().fetch();
     } catch (e) {
+      // TODO: to test this
+      await reSignin();
       console.log(e);
     }
   }
